7130d72be06f5da12b1c670c2c1171ed24ac56df
nuocbiendang12 <86813867+nuocbiendang12@users.noreply.github.com>
2021-12-31 20:45:47 +0700

    item cho personal page

--- lib/widgets/personal/library_playlist_card.dart
+++ lib/widgets/personal/library_playlist_card.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
 class LibraryPlaylistCard extends StatelessWidget {
-  const LibraryPlaylistCard({Key? key}) : super(key: key);
+  const LibraryPlaylistCard(
+      {Key? key,
+      required this.title,
+      required this.iconAsset,
+      required this.width})
+      : super(key: key);
+
+  final String title;
+  final String iconAsset;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: width,
+      child: Card(
+          elevation: 3,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  iconAsset,
+                  width: 32,
+                  height: 32,
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
