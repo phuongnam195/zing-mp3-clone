23bfd8886e4e61dfd6ab35f9e99d9a59bfd89f0c
Hoang Manh Khiem <91199095+Nanashi0it@users.noreply.github.com>
2021-12-31 10:56:59 +0700

    SearchScreen (chưa thể xem lại recent sau khi tìm kiếm kết quả)

--- lib/screens/common/search_screen.dart
+++ lib/screens/common/search_screen.dart
@@ -1,46 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:zing_mp3_clone/controller/player_controller.dart';
+import 'package:zing_mp3_clone/providers/music_provider.dart';
+import 'package:zing_mp3_clone/widgets/common/music_card.dart';
 
 import '../../providers/recent_search_provider.dart';
 import '../../models/music.dart';
 import '../../widgets/search/search_box.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   static const routeName = '/home/search';
 
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  List<Music> recentList = [];
+  List<Music>? resultList;
+
+  @override
+  void initState() {
+    super.initState();
+    recentList = RecentSearchProvider.instance.list;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final recentSearchs = RecentSearchProvider.instance;
-
-    // TODO: Hiện danh sách này lên body
-    // Danh sách các bài hát tìm kiếm gần đây
-    final List<Music> recentList = recentSearchs.list;
-
-    // Sau khi nhập từ khóa và ấn Enter thì danh sách kết quả
-    // hiện ra, đẩy danh sách tìm kiếm gần đây xuống dưới
-    // Hàm lấy danh sách bài hát từ keyword nhập vào: AllMusic.instance.search(keyword)
-
-    // Sử dụng widget MusicCard để thể hiện một bài hát (cả gần đây và kết quả): xem file widgets/common/music_card.dart
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          color: Colors.black,
-          icon: const Icon(Icons.keyboard_backspace_rounded),
-        ),
-        titleSpacing: 0,
-        title: const Padding(
-          padding: EdgeInsets.only(right: 20),
-          child: SearchBox(
-            autofocus: true,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            color: Colors.black,
+            icon: const Icon(Icons.keyboard_backspace_rounded),
+          ),
+          titleSpacing: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: SearchBox(
+              autofocus: true,
+              onSubmitted: (value) {
+                setState(() {
+                  resultList = MusicProvider.instance.search(value);
+                });
+              },
+            ),
           ),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: resultList == null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Tìm kiếm gần đây',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(height: 5),
+                    for (var music in recentList)
+                      MusicCard(
+                          title: music.title,
+                          artists: music.artists,
+                          thumbnailUrl: music.thumbnailUrl)
+                  ],
+                )
+              : ListView.builder(
+                  itemCount: resultList!.length,
+                  itemBuilder: (ctx, i) {
+                    final music = resultList![i];
+                    return MusicCard(
+                      title: music.title,
+                      artists: music.artists,
+                      thumbnailUrl: music.thumbnailUrl,
+                      onTap: () {
+                        final controller = PlayerController.instance;
+                        if (!controller.isActive) {
+                          controller.maximizeScreen(context);
+                        }
+                        controller.setMusic(music);
+                        RecentSearchProvider.instance.add(music.id);
+                      },
+                    );
+                  }),
+        ));
   }
 }
