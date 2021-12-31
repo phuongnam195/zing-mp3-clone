a57a4bb86684e2b8898a4bc53b619d1b13be4643
Hoang Manh Khiem <91199095+Nanashi0it@users.noreply.github.com>
2021-12-31 10:55:03 +0700

    Màn hình Account

--- lib/screens/common/account_screen.dart
+++ lib/screens/common/account_screen.dart
@@ -11,27 +11,81 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget settingItem(String title, IconData icon, void Function() onTap) {
+      return ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.black,
+          size: 28,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+        onTap: onTap,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          icon: Icon(Icons.keyboard_backspace_rounded),
+          icon: const Icon(
+            Icons.keyboard_backspace_rounded,
+            color: Colors.black,
+          ),
         ),
-        title: Text('Tài khoản cá nhân'),
+        titleSpacing: 0,
+        title: const Text(
+          'Tài khoản cá nhân',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Column(
         children: [
-          Text(Config.instance.myAccount!.name),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .popUntil(ModalRoute.withName(WelcomeScreen.routeName));
-
-                FirebaseAuth.instance.signOut();
-              },
-              child: Text('ĐĂNG XUẤT'))
+          Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Color(0xFF65509E), Color(0xFF8947AD)])),
+              child: ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  leading: Container(
+                    width: 46,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/account/avatar.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(23),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2.5,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    Config.instance.myAccount!.name,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ))),
+          const SizedBox(height: 40),
+          settingItem('Đăng xuất', Icons.logout, () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+                WelcomeScreen.routeName, (Route<dynamic> route) => false);
+            FirebaseAuth.instance.signOut();
+          }),
+          settingItem('Giới thiệu', Icons.info_outline_rounded, () {}),
         ],
       ),
     );
