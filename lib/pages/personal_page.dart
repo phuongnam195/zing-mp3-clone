6a360396f3618dfbf76931950f51b3d2fdd3c461
nuocbiendang12 <86813867+nuocbiendang12@users.noreply.github.com>
2021-12-31 20:47:22 +0700

    personal page, only UI (chưa làm playlist recent)

--- lib/pages/personal_page.dart
+++ lib/pages/personal_page.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
 
+import '../config.dart';
+import '../widgets/personal/personal_playlist_card.dart';
+import '../widgets/personal/library_playlist_card.dart';
+
 class PersonalPage extends StatelessWidget {
   const PersonalPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenWidth = MediaQuery.of(context).size.width;
+    const HORIZ_PADDING = 20.0;
+    const GAP = 8.0;
+
+    final userPlaylists = Config.instance.myAccount?.userPlaylists;
+
+    // lpc = LibraryPlaylistCard
+    final lpcWidth = (screenWidth - 2 * HORIZ_PADDING - GAP) / 2;
+
+    return Padding(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          left: HORIZ_PADDING,
+          right: HORIZ_PADDING),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            const Text('Thư viện',
+                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+            const SizedBox(height: GAP),
+            Row(
+              children: [
+                LibraryPlaylistCard(
+                  title: 'Tất cả bài hát',
+                  iconAsset: 'assets/icons/music_library_48.png',
+                  width: lpcWidth,
+                ),
+                const SizedBox(width: GAP),
+                LibraryPlaylistCard(
+                    title: 'Trên thiết bị',
+                    iconAsset: 'assets/icons/ipod_old_48.png',
+                    width: lpcWidth),
+              ],
+            ),
+            const SizedBox(height: GAP),
+            LibraryPlaylistCard(
+              title: 'Đã tải',
+              iconAsset: 'assets/icons/download_48.png',
+              width: lpcWidth,
+            ),
+            if (userPlaylists != null)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 25),
+                  const Text('Playlist của tôi',
+                      style:
+                          TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: GAP),
+                  for (var playlist in userPlaylists)
+                    PersonalPlaylistCard(playlist),
+                ],
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
