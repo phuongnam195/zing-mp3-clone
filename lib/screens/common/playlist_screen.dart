39567578eb4f68fb759399dffbc2495c3a84348f
HuynhThanhMy <74922520+HuynhThanhMy@users.noreply.github.com>
2022-01-01 14:59:49 +0700

    Màn hình Playlist

--- lib/screens/common/playlist_screen.dart
+++ lib/screens/common/playlist_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
+import '../../widgets/common/music_card.dart';
 import '../../controller/player_controller.dart';
 import '../../providers/playlist_provider.dart';
 import '../../models/music.dart';
@@ -14,48 +17,126 @@ class PlaylistScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final playerController = PlayerController.instance;
 
-    // Dữ liệu giả
-    final Playlist playlist = PlaylistProvider.instance.list[0];
-    final String title = playlist.title;
-    final int numberOfMusic = playlist.musicIDs.length;
-    final String backgroundImageUrl = playlist.getMusicAtIndex(0).thumbnailUrl;
+    late final Playlist playlist;
+    late final String title;
+    late final int numberOfMusic;
 
-    return Scaffold(
-        body: Column(children: [
-      Stack(
-        children: [
-          // WIDGET => Ảnh nền: làm mờ (tham khảo trong playing_screen.dart: BackdropFilter), chú ý size
-          Column(
-            children: [
-              // WIDGET => Row: Nút back, tiêu đề, số lượng
-              // WIDGET => Container bo tròn 2 góc trên, chứa nút PHÁT NGẪU NHIÊN
-              ElevatedButton(
-                  onPressed: () {
-                    if (!playerController.isActive) {
-                      playerController.maximizeScreen(context);
-                    }
-                    playerController.setShufflePlaylist(playlist);
-                    playerController.notifyChange();
-                  },
-                  child: Text('PHÁT NGẪU NHIÊN')),
-            ],
-          )
-        ],
-      ),
-      FutureBuilder<List<Music>>(
-        future: playlist.getMusicList(),
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+    final arguments =
+        ModalRoute.of(context)!.settings.arguments! as Map<String, String>;
+    final type = arguments['type'];
+    if (type == 'ExplorerPlaylist') {
+      final playlistId = arguments['id']!;
+      playlist = PlaylistProvider.instance.getByID(playlistId);
+      title = playlist.title;
+      numberOfMusic = playlist.musicIDs.length;
+    }
 
-          final List<Music> musicList = snapshot.data!;
+    return Stack(
+      children: [
+        type == 'ExplorerPlaylist'
+            ? Container(
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: NetworkImage(playlist.imageUrl!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                    child: Container(
+                      color: Colors.black.withOpacity(0.02),
+                    )),
+              )
+            : Image.asset('assets/images/playlist/user_playlist_background.jpg',
+                fit: BoxFit.fitWidth),
+        Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.keyboard_backspace_rounded,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              title: Text(
+                title + ' (' + numberOfMusic.toString() + ')',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(color: Colors.white, fontSize: 17),
+              ),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 20, bottom: 12),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                  ),
+                  child: Center(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor,
+                          shape: const StadiumBorder(),
+                          elevation: 0,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 12),
+                        ),
+                        onPressed: () {
+                          playerController.maximizeScreen(context);
+                          playerController.setShufflePlaylist(playlist);
+                        },
+                        child: const Text('PHÁT NGẪU NHIÊN',
+                            style: TextStyle(fontSize: 15))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: FutureBuilder<List<Music>>(
+                      future: playlist.getMusicList(),
+                      builder: (ctx, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
 
-          // Hiện danh sách: Thay Container bằng ListView.builder()
-          // Cách dùng ListView.builder(): Tham khảo trong radio_page.dart
-          return Container();
-        },
-      ),
-    ]));
+                        final musicList = snapshot.data!;
+                        return ListView.builder(
+                          itemCount: numberOfMusic,
+                          itemBuilder: (ctx, index) {
+                            final music = musicList[index];
+                            return MusicCard(
+                              title: music.title,
+                              artists: music.artists,
+                              thumbnailUrl: music.thumbnailUrl,
+                              onTap: () {
+                                if (!playerController.isActive) {
+                                  playerController.maximizeScreen(context);
+                                }
+                                playerController.setMusic(music);
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ))
+      ],
+    );
   }
 }
